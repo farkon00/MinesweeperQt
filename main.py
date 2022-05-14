45bc45e08ec2641bb32e801cb3f860d9f11e5d7f
farkon00 <sammer2016sammer@gmail.com>
2022-05-14 17:02:22 +0300

    Added toolbar

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-FIELD_WIDTHS: list[tuple[int, int]] = [
-    (8, 16),
-    (16, 32),
+MODES: list[tuple[int, int]] = [
+    (8, 10),
+    (16, 40),
     (24, 99)
 ]
 
@@ -18,7 +18,43 @@ class MainWindow(QtWidgets.QMainWindow):
     """
     def __init__(self) -> None:
         super().__init__()
-        self.setGeometry(300, 300, 300, 300)
+
+        self.level = MODES[0]
+        self.level_index = 0
+
+        self.init_ui()
+
+        self.setWindowTitle("Minesweeper")
+        self.setFixedSize(600, 600)
+        self.show()
+
+    def init_ui(self) -> None:
+        """
+        Initializes the UI
+        """
+        self.toolbar_widget = QtWidgets.QWidget()
+        self.toolbar_layout = QtWidgets.QHBoxLayout()
+
+        self.mines_label = QtWidgets.QLabel(f"Mines left: {self.level[1]}")
+        self.mines_label.setFont(QtGui.QFont("Arial", 20))
+        self.mines_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.toolbar_layout.addWidget(self.mines_label)
+
+        self.restart_button = QtWidgets.QPushButton()
+        self.restart_button.setFixedSize(32, 32)
+        self.restart_button.setIconSize(QtCore.QSize(32, 32))
+        self.restart_button.setIcon(QtGui.QIcon("images/smiley.png"))
+        self.restart_button.setFlat(True)
+        self.toolbar_layout.addWidget(self.restart_button)
+
+        self.clock = QtWidgets.QLabel("Timer: 0")
+        self.clock.setFont(QtGui.QFont("Arial", 20))
+        self.clock.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.toolbar_layout.addWidget(self.clock)
+
+        self.toolbar_widget.setLayout(self.toolbar_layout)
+
+        self.setCentralWidget(self.toolbar_widget)
 
 def main() -> None:
     """
@@ -26,7 +62,6 @@ def main() -> None:
     """
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
-    window.show()
     app.exec_()
 
 if __name__ == "__main__":
