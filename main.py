212b66b7b9c8b383b0bf131847bb097ae3c3cf33
farkon00 <sammer2016sammer@gmail.com>
2022-05-14 16:31:05 +0300

    Fixed mypy, small code changes

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-from PyQt5 import QtWidgets, QtCore, QtGui
+"""
+Minesweeper game in Python pyQt5
+"""
 
-def main():
-    pass
+import sys
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+FIELD_WIDTHS: list[tuple[int, int]] = [
+    (8, 16),
+    (16, 32),
+    (24, 99)
+]
+
+class MainWindow(QtWidgets.QMainWindow):
+    """
+    Main window of minesweeper game
+    """
+    def __init__(self) -> None:
+        super().__init__()
+        self.setGeometry(300, 300, 300, 300)
+
+def main() -> None:
+    """
+    Runs the minesweeper game
+    """
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec_()
 
 if __name__ == "__main__":
     main()
