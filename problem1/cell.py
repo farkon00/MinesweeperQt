a895a4f379fd57fd88a71b28a221f01b0ea6c210
farkon00 <sammer2016sammer@gmail.com>
2022-05-28 16:32:03 +0300

    Problem 1 solved

---
+++ problem1/cell.py
@@ -0,0 +1,87 @@
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtCore import *
+from state import Statuses 
+
+
+class Cell(QtWidgets.QPushButton):
+    MINE_COLORS = [
+        Qt.GlobalColor.blue, Qt.GlobalColor.darkGreen, Qt.GlobalColor.red, Qt.GlobalColor.darkBlue,
+        Qt.GlobalColor.darkRed , Qt.GlobalColor.green, Qt.GlobalColor.black, Qt.GlobalColor.lightGray
+    ]
+
+    def __init__(self, x, y, grid, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._x = x
+        self._y = y
+
+        self.grid = grid 
+
+        self.is_bomb = self.is_revealed = self.is_flagged = False
+
+    def paintEvent(self, event):
+        """
+        Renders cell
+        """
+        p = QtGui.QPainter(self)
+        p.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing)
+        r = event.rect()
+        outer, inner = Qt.GlobalColor.gray, Qt.GlobalColor.lightGray
+        if self.is_revealed:
+            if self.is_bomb:
+                p.fillRect(r, QtGui.QBrush(Qt.GlobalColor.red))
+            else:
+                p.fillRect(r, QtGui.QBrush(Qt.GlobalColor.transparent))
+        elif self.is_flagged:
+            p.fillRect(r, QtGui.QBrush(Qt.GlobalColor.blue))
+        else:
+            p.fillRect(r, QtGui.QBrush(inner))
+        pen = QtGui.QPen(outer)
+        pen.setWidth(1)
+        p.setPen(pen)
+        p.drawRect(r)
+
+        around = self.grid.count_mines_around(self._x, self._y)
+        if self.is_revealed and not self.is_bomb and around > 0:
+            self.setStyleSheet(f"color: red; font-weight: bold;")
+            pen = QtGui.QPen(self.MINE_COLORS[around - 1])
+            pen.setWidth(1)
+            p.setPen(pen)
+            p.drawText(r, Qt.AlignmentFlag.AlignCenter, str(around))
+
+        if self.is_revealed and self.is_bomb:
+            self.setStyleSheet("font-weight: bold;")
+            p.drawText(r, Qt.AlignmentFlag.AlignCenter, "X")
+
+    def mouseReleaseEvent(self, event) -> None:
+        """Handler for click event"""
+
+        if self.grid.state.status == Statuses.LOST or self.grid.state.status == Statuses.WON:
+            return 
+
+        if event.button() == Qt.MouseButton.LeftButton:
+            if self.is_flagged:
+                return
+
+            if self.is_revealed:
+                if self.grid.is_flags_right(self._x, self._y):
+                    self.grid.reveal_around(self._x, self._y)
+                    self.grid.check_win()
+            else:
+                self.grid.reveal_cells(self._x, self._y)
+                if self.is_bomb:
+                    self.grid.state.status = Statuses.LOST
+
+                self.grid.place_mines()
+        elif event.button() == Qt.MouseButton.RightButton:
+            if self.is_revealed:
+                return
+
+            if not self.is_flagged:
+                self.grid.state.flag()
+                self.is_flagged = True
+            else:
+                self.grid.state.unflag()
+                self.is_flagged = False
+
+        self.update()
---
+++ problem1/grid.py
@@ -0,0 +1,109 @@
+import random
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from cell import Cell
+from state import *
+
+class Grid(QtWidgets.QGridLayout):
+    """
+    Grid of minesweeper game
+    """
+    def __init__(self, state: State, size: int, bombs: int, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.state = state
+
+        self._size = size
+        self.bombs = bombs
+
+        self.grid: list[list[Cell]] = []
+        self.is_placed: bool = False
+
+        for i in range(self._size):
+            self.grid.append([])
+            for j in range(self._size):
+                self.grid[-1].append(Cell(i, j, self))
+                self.addWidget(self.grid[-1][-1], i, j)
+
+    def count_mines_around(self, x, y):
+        """
+        Counts the number of mines around the cell
+        """
+        count = 0
+        for i in range(x-1, x+2):
+            for j in range(y-1, y+2):
+                if i >= 0 and i < self._size and j >= 0 and j < self._size:
+                    if self.grid[i][j].is_bomb:
+                        count += 1
+        return count
+
+    def is_flags_right(self, x: int, y: int) -> bool:
+        """
+        Checks if flags around the cell is right
+        """
+        for i in range(x-1, x+2):
+            for j in range(y-1, y+2):
+                if i >= 0 and i < self._size and j >= 0 and j < self._size:
+                    if self.grid[i][j].is_bomb and not self.grid[i][j].is_flagged:
+                        return False
+                    elif not self.grid[i][j].is_bomb and self.grid[i][j].is_flagged:
+                        return False
+        return True
+
+    def check_win(self):
+        flag = []
+        for i in self.grid:
+            for j in i:
+                if not (j.is_bomb or j.is_revealed):
+                    return
+                elif j.is_revealed and not j.is_flagged:
+                    flag.append(j)
+
+        self.state.status = Statuses.WON
+        for i in flag:
+            j.is_flagged = True    
+            j.update() 
+
+    def reveal_around(self, x: int, y: int):
+        """Reveals all cells around the cell"""
+        for i in range(x-1, x+2):
+            for j in range(y-1, y+2):
+                if i >= 0 and i < self._size and j >= 0 and j < self._size:
+                    if not self.grid[i][j].is_revealed and not self.grid[i][j].is_bomb and not self.grid[i][j].is_flagged:
+                        self.grid[i][j].is_revealed = True
+                        self.grid[i][j].update()
+
+    def reveal_cells(self, x: int, y: int) -> None:
+        """
+        Reveals all cells around the cell, if they should be revealed
+        Checks for win
+        """
+        self.grid[x][y].is_revealed = True
+        if self.count_mines_around(x, y) != 0:
+            self.grid[x][y].update()
+            self.check_win()
+            return
+        for i in range(x-1, x+2):
+            for j in range(y-1, y+2):
+                if i >= 0 and i < self._size and j >= 0 and j < self._size:
+                    if not self.grid[i][j].is_revealed and not self.grid[i][j].is_bomb and not self.grid[i][j].is_flagged:
+                        self.grid[i][j].is_revealed = True
+                    self.grid[i][j].update()
+        self.check_win()
+
+    def place_mines(self) -> None:
+        """Places mines on the grid, will make all checks by itself"""
+        if self.is_placed:
+            return
+
+        self.state.status = Statuses.PLAYING
+        placed = 0
+        while placed < self.bombs:
+            x = random.randint(0, self._size-1)
+            y = random.randint(0, self._size-1)
+            if not self.grid[x][y].is_bomb and not self.grid[x][y].is_revealed:
+                self.grid[x][y].is_bomb = True
+                placed += 1
+                self.grid[x][y].update()
+
+        self.is_placed = True
